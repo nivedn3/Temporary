22f328ab3259bd607fef8aa08e999f204c69dea0
Ubuntu <ubuntu@ip-172-31-19-221.ap-southeast-1.compute.internal>
2016-03-30 10:04:20 +0000

    first commit

---
+++ cd/models.py
@@ -0,0 +1,57 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class request_conf(models.Model):
+	Cus_link=models.URLField()
+	Ser_product=models.CharField(max_length=50)
+	Ser_image=models.URLField()
+	cus_category=models.CharField(max_length=50,default=1)
+	Cus_email=models.CharField(max_length=50,null=True)
+
+	Ser_price=models.CharField(max_length=50)
+	#Q_price=models.CharField(max_length=50)
+	#Sel_type=models.IntegerField(default=False)
+	#Sel_comments=models.CharField(max_length=500)
+	Cus_expiry=models.IntegerField(null=True)
+	Cus_id=models.CharField(max_length=30,primary_key=True)
+	#Sel_deltype=models.IntegerField(default=False)
+	#Cus2_conf=models.IntegerField(default=False)
+	#Sel2_conf=models.IntegerField(default=False)
+	#img_path=models.CharField(max_length=100,null=True)
+	cus_loc=models.CharField(max_length=100,null=True)
+	Cus_name=models.CharField(max_length=100,null=True)
+
+class sellerlogindb(models.Model):
+	user=models.CharField(max_length=50)
+	password=models.CharField(max_length=50)
+	email=models.EmailField(primary_key=True)
+	mobile=models.BigIntegerField()
+	shopname=models.CharField(max_length=80)
+	shopid=models.CharField(max_length=80)
+	office_no=models.IntegerField(null=True)
+	city=models.CharField(max_length=200)
+	Address1=models.CharField(max_length=200)
+	Address2=models.CharField(max_length=200)
+	sel_loc=models.CharField(max_length=100,null=True)
+	category=models.BigIntegerField(default=1)
+	gcmid=models.CharField(max_length=1000)
+	token=models.CharField(max_length=100)
+	gps=models.CharField(max_length=100)
+        imagepath=models.CharField(max_length=200,null=True)
+class selldb(models.Model):
+	Cus_id=models.CharField(max_length=30) 
+	Q_price=models.CharField(max_length=50)
+	Sel_type=models.IntegerField(default=False)
+	Sel_comments=models.CharField(max_length=500)
+ 	Sel_deltype=models.IntegerField(default=False)
+	Cus2_conf=models.IntegerField(default=False)
+	Sel2_conf=models.IntegerField(default=False)
+	img_path=models.CharField(max_length=100,null=True)
+	Sel_id=models.CharField(max_length=30,primary_key=True)
+
+
+
+	
+	
